e839c0f8664ea3ba284b12f2587048c23e1113d5
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-04-10 21:17:23 +0700

    menambahkan kode pada route\web.php
    menambahkan fungsi CRUD seperti
    - index()
    - create()
    - store()
    - show()
    - edit()
    - update()
    destroy()

--- siakad/routes/web.php
+++ siakad/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Http\Request;
+
+Route::resource('mahasiswa', MahasiswaController::class);
 
 /*
 |--------------------------------------------------------------------------
